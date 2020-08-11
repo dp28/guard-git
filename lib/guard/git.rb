9fc618cab70979a1e27dd004e49da46b3a5e4553
Daniel Patterson <dan24patt@gmail.com>
2020-08-10 22:46:13 -0400

    Ignore file changes when changing branch

--- lib/guard/git.rb
+++ lib/guard/git.rb
@@ -10,7 +10,45 @@ module Guard
 
     module DslExtensions
       def watch(pattern, &action)
-        super(pattern, &action)
+        super(Matcher.new(pattern), &action)
+      end
+    end
+
+    class Matcher
+      def initialize(pattern)
+        @pattern = pattern
+      end
+
+      def match(filename_or_pathname)
+        path = filename_or_pathname.to_s
+        result = @pattern.match(path)
+        return nil if result.nil? || skip_file?(path)
+
+        result
+      end
+
+      private
+
+      def skip_file?(path)
+        !changed?(path) && !always_allowed?(path)
+      end
+
+      def changed?(path)
+        changed_files.include?(path)
+      end
+
+      def always_allowed?(path)
+        ignored_files.include?(path)
+      end
+
+      def ignored_files
+        `git ls-files --others -i --exclude-standard`.lines.map(&:chomp)
+      end
+
+      def changed_files
+        `git status --porcelain`.lines.map do |line|
+          line.chomp.split(' ').last
+        end
       end
     end
   end
